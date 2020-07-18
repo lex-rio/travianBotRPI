7c32a228a0bceb58656a259c969ec1708bd480eb
alexandr.orishchuk <alexandr.orishchuk@ys.bet>
2020-07-18 19:40:49 +0300

    add no sendbox option for raspberry

--- index.js
+++ index.js
@@ -3,11 +3,21 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://ru4.kingdoms.com');
-  await page.screenshot({path: 'example.png'});
+        try {
+                const browser = await puppeteer.launch({
+                      headless: true,
+                      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+                });
+                const page = await browser.newPage();
+                await page.goto('https://ru4.kingdoms.com');
+                await page.screenshot({path: 'example.png'});
 
-  await browser.close();
+                await browser.close();
+                console.log('done');
+                  
+        } catch (e) {
+                    console.log(e)
+                  
+        }
         
 })();
