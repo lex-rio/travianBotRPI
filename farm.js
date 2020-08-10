685c7ee5c88691c527ae8d72ec6f70e66e81894a
alexandr.orishchuk <alexandr.orishchuk@ys.bet>
2020-08-10 17:49:17 +0300

    refactoring

--- farm.js
+++ farm.js
@@ -3,7 +3,6 @@
 const telegram = require('./telegram')
 const fetch = require('./fetch')
 const playersFarmLists = require('./farmLists.json')
-
 const options = {
   hostname: 'ru4.kingdoms.com',
   port: 443,
@@ -14,23 +13,32 @@ const options = {
   }
 }
 
-Object.values(playersFarmLists).map(({villages, chatId, name, session}) => {
-  villages.map(village => {
-    setInterval(() => {
-      fetch(options, {
-        "controller":"troops",
-        "action":"startPartialFarmListRaid",
-        "params": {
-          "entryIds":[ 50069 ],
-          "listId": village.listId,
-          "villageId": village.id
-        },
-        "session": session
-      }).then(data => {
-        telegram.log(chatId, `${name} ${village.name} ${encodeURIComponent(JSON.stringify(data.cache[1].data.units))}`)
-      }).catch(error => {
-        telegram.log(chatId, `${name} ${village.name} ${encodeURIComponent(JSON.stringify(error))}`)
-      })
-    }, village.period * 60000)
+const sendFarm = ({village, player}) => {
+  fetch(options, {
+    "controller":"troops",
+    "action":"startPartialFarmListRaid",
+    "params": {
+      "entryIds":[ 50069 ],
+      "listId": village.listId,
+      "villageId": village.id
+    },
+    "session": player.session
+  }).then(data => {
+    if (data.error) {
+      throw data.error
+    }
+    telegram.log(player.chatId, `${player.name} ${village.name} ${encodeURIComponent(JSON.stringify(data.cache[1].data.units))}`)
+  }).catch(error => {
+    telegram.log(player.chatId, `${player.name} ${village.name} error ${encodeURIComponent(JSON.stringify(error))}`)
+  })
+}
+
+Object.values(playersFarmLists).map(player => {
+  player.villages.map(village => {
+    sendFarm({village, player})
+    setInterval(
+      () => sendFarm({village, player}),
+      village.period * 60000
+    )
   })
 })
