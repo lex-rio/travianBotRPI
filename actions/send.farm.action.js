242f907725f32be8478821cda293fd5d4bbaf883
alexandr.oryshchuk <alexandr.orishchuk@ys.bet>
2021-02-01 13:59:01 +0200

    update farm

--- actions/send.farm.action.js
+++ actions/send.farm.action.js
@@ -6,7 +6,7 @@ class SendFarmAction extends Action {
 
   constructor(data, callbacks) {
     super(data, callbacks)
-    this.period = data.period || 300
+    this.period = data.period || 240
     this.actionName = 'startFarmListRaid'
     this.controller = 'troops'
     this.action = 'startFarmListRaid'
@@ -14,7 +14,7 @@ class SendFarmAction extends Action {
 
   params() {
     return {
-      listIds: [53],
+      listIds: [857],
       villageId: 536068116
     }
   }
